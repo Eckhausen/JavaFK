111c64615078ad4b867ad9afbc7cc8bad417a7fc
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-11-15 23:33:23 +0100

    Update

--- lab4/src/testqueue/TestAppendFifoQueue.java
+++ lab4/src/testqueue/TestAppendFifoQueue.java
@@ -17,26 +17,25 @@ import java.util.Queue;
 import java.util.Iterator;
 
 class TestAppendFifoQueue {
-	private Queue<Integer> myIntQueue;
-	private Queue<String> myStringQueue;
-    private Queue<Integer> myIntQueue2;
-    private FifoQueue<Integer> list1;
+	private FifoQueue<Integer> list1;
     private FifoQueue<Integer> list2;
+	private Queue<String> myStringQueue;
+    
+    
 
 	@BeforeEach
 	void setUp() {
-		myIntQueue = new FifoQueue<Integer>();
-		myStringQueue = new FifoQueue<String>();
-        myIntQueue2 = new FifoQueue<Integer>();
-        list1 = new FifoQueue<Integer>();
+		list1 = new FifoQueue<Integer>();
         list2 = new FifoQueue<Integer>();
+		myStringQueue = new FifoQueue<String>();
+        
 	}
 
 	@AfterEach
 	void tearDown(){
-		myIntQueue = null;
+		list1 = null;
+        list2 = null;
 		myStringQueue = null;
-        myIntQueue2 = null;
 	}
 
 	/**
@@ -44,17 +43,17 @@ class TestAppendFifoQueue {
 	 */
 	@Test
 	void testNewFifoQueue() {
-		assertTrue(myIntQueue.isEmpty(), "Wrong result from empty of queue");
-		assertEquals(0, myIntQueue.size(), "Wrong size of empty queue");
+		assertTrue(list1.isEmpty(), "Wrong result from empty of queue");
+		assertEquals(0, list1.size(), "Wrong size of empty queue");
 	}
 
 	/** Test a single offer followed by a single peek. */
 	@Test
 	void testPeek() {
-		myIntQueue.offer(1);
-		int i = myIntQueue.peek();
+		list1.offer(1);
+		int i = list1.peek();
 		assertEquals(1, i, "peek on queue of size 1");
-		assertEquals(1, myIntQueue.size(), "peek on queue of size 1");
+		assertEquals(1, list1.size(), "peek on queue of size 1");
 	}
 
 	/**
@@ -62,10 +61,10 @@ class TestAppendFifoQueue {
 	 */
 	@Test
 	void testPoll() {
-		myIntQueue.offer(1);
-		int i = myIntQueue.poll();
+		list1.offer(1);
+		int i = list1.poll();
 		assertEquals(1, i, "poll on queue of size 1");
-		assertEquals(0, myIntQueue.size(), "Wrong size after poll");
+		assertEquals(0, list1.size(), "Wrong size after poll");
 	}
 
 	/**
@@ -73,7 +72,7 @@ class TestAppendFifoQueue {
 	 */
 	@Test
 	void testPeekOfEmpty() {
-		assertEquals(null, myIntQueue.peek(), "Front of empty queue not null");
+		assertEquals(null, list1.peek(), "Front of empty queue not null");
 	}
 
 	/**
@@ -81,7 +80,7 @@ class TestAppendFifoQueue {
 	 */
 	@Test
 	void testPollOfEmpty() {
-		assertEquals(null, myIntQueue.poll(), "Poll of empty queue should return null") ;
+		assertEquals(null, list1.poll(), "Poll of empty queue should return null") ;
 	}
 
 	/**
@@ -105,16 +104,16 @@ class TestAppendFifoQueue {
 	 */
 	@Test
 	void testOrder() {
-		myIntQueue.offer(1);
-		myIntQueue.offer(2);
-		myIntQueue.offer(3);
-		myIntQueue.offer(4);
-		myIntQueue.offer(5);
+		list1.offer(1);
+		list1.offer(2);
+		list1.offer(3);
+		list1.offer(4);
+		list1.offer(5);
 		for (int i = 1; i <= 5; i++) {
-			int k = myIntQueue.poll();
+			int k = list1.poll();
 			assertEquals(i, k, "poll returns incorrect element");
 		}
-		assertTrue(myIntQueue.isEmpty(), "Queue not empty");
+		assertTrue(list1.isEmpty(), "Queue not empty");
 	}
 
 	/**
@@ -122,21 +121,21 @@ class TestAppendFifoQueue {
 	 */
 	@Test
 	void testMakeQueueEmpty() {
-		myIntQueue.offer(1);
-		myIntQueue.offer(2);
-		myIntQueue.poll();
-		myIntQueue.poll();
-		assertEquals(0,  myIntQueue.size(), "Wrong size after poll");
-		assertTrue(myIntQueue.isEmpty(), "Queue not empty after poll");
-		myIntQueue.offer(3);
-		myIntQueue.offer(4);
-		assertEquals(2, myIntQueue.size(), "Wrong size after offer");
+		list1.offer(1);
+		list1.offer(2);
+		list1.poll();
+		list1.poll();
+		assertEquals(0,  list1.size(), "Wrong size after poll");
+		assertTrue(list1.isEmpty(), "Queue not empty after poll");
+		list1.offer(3);
+		list1.offer(4);
+		assertEquals(2, list1.size(), "Wrong size after offer");
 		for (int i = 3; i <= 4; i++) {
-			int k = myIntQueue.poll();
+			int k = list1.poll();
 			assertEquals(i, k, "poll returns incorrect element");
 		}
-		assertEquals(0, myIntQueue.size(), "Wrong size after poll");
-		assertTrue(myIntQueue.isEmpty(), "Queue not empty after poll");
+		assertEquals(0, list1.size(), "Wrong size after poll");
+		assertTrue(list1.isEmpty(), "Queue not empty after poll");
 	}
 
 	/**
@@ -144,7 +143,7 @@ class TestAppendFifoQueue {
 	 */
 	@Test
 	void testIteratorEmptyQueue() {
-		Iterator<Integer> itr = myIntQueue.iterator();
+		Iterator<Integer> itr = list1.iterator();
 		assertFalse(itr.hasNext(), "Wrong result from next in empty queue");
 		assertThrows(NoSuchElementException.class, () -> itr.next());
 	}
@@ -156,9 +155,9 @@ class TestAppendFifoQueue {
 	void testIteratorNonEmptyQueue() {
 		int nbr = 5;
 		for (int i = 1; i <= nbr; i++) {
-			myIntQueue.offer(i);
+			list1.offer(i);
 		}
-		Iterator<Integer> itr = myIntQueue.iterator();
+		Iterator<Integer> itr = list1.iterator();
 		assertTrue(itr.hasNext(), "Wrong result from hasNext");
 		for (int i = 1; i <= nbr; i++) {
 			assertTrue(itr.hasNext(), "Wrong result from hasNext");
@@ -176,13 +175,13 @@ class TestAppendFifoQueue {
 	void testThreeIterators() {
 		int nbr = 5;
 		for (int i = 1; i <= nbr; i++) {
-			myIntQueue.offer(i);
+			list1.offer(i);
 		}
 
 		// first, set up two iterators at the same time, and check that they both work
 
-		Iterator<Integer> itr1 = myIntQueue.iterator();
-		Iterator<Integer> itr2 = myIntQueue.iterator();
+		Iterator<Integer> itr1 = list1.iterator();
+		Iterator<Integer> itr2 = list1.iterator();
 		
 		for (int i = 1; i <= nbr; i++) {
 			assertTrue(itr1.hasNext(), "Wrong result from itr1.hasNext");
@@ -195,7 +194,7 @@ class TestAppendFifoQueue {
 
 		// then, test a third iterator after the previous iterations
 		
-		Iterator<Integer> itr3 = myIntQueue.iterator();
+		Iterator<Integer> itr3 = list1.iterator();
 		for (int i = 1; i <= nbr; i++) {
 			assertTrue(itr3.hasNext(), "Wrong result from itr3.hasNext");
 			assertEquals(Integer.valueOf(i), itr3.next(), "Wrong result from itr3.next");
@@ -204,31 +203,66 @@ class TestAppendFifoQueue {
 
     @Test
     void two_empty_queues() {
-        assertEquals(0, myIntQueue.size(), "Wrong size for queue 1");
-        assertEquals(0, myIntQueue2.size(), "Wrong size for queue 1");
-        myIntQueue.append(myIntQueue2);
+        assertEquals(0, list1.size(), "Wrong size for queue 1");
+        assertEquals(0, list2.size(), "Wrong size for queue 1");
+        list1.append(list2);
         
-        assertTrue(myIntQueue.isEmpty(), "Queue 1 not empty.");
-        assertTrue(myIntQueue2.isEmpty(), "Queue 2 not empty.");
+        assertTrue(list1.isEmpty(), "Queue 1 not empty.");
+        assertTrue(list2.isEmpty(), "Queue 2 not empty.");
     }
 
     @Test
     void empty_queue_appended_to_non_empty_queue(){
+        list1.offer(1);
+        list1.offer(2);
+        assertFalse(list1.isEmpty(), "Queue 1 is empty"); //Ska inte va tom
+        assertTrue(list2.isEmpty(), "Queue 2 is empty"); //Ska va tom
+        
+        list1.append(list2);
+        assertEquals(2, list1.size(), "Wrong size");
+        
+        assertEquals(1, list1.poll(), "sadasdasd");
+        assertEquals(2, list1.poll(), "sadasdasd");
+        assertEquals(null, list1.poll(), "sadasdasd");
+        assertEquals(0, list1.size(), "Wrong size");
+
 
     }
 
     @Test
     void non_empty_queue_appended_to_empty_queue(){
+        list2.offer(1);
+        list2.offer(2);
+        list1.append(list2);
         
+        assertEquals(1, list1.poll());
+        assertEquals(2, list1.poll());
+        assertEquals(null, list1.poll());
+        assertTrue(list2.isEmpty(),"List 2 is NOT empty.");
+        assertEquals(0, list1.size()); 
     }
 
     @Test
     void two_non_empty_queues(){
+        list1.offer(1);
+        list1.offer(2);
+        list2.offer(3);
+        list2.offer(4);
+        list1.append(list2);
+
+        assertEquals(1, list1.poll());
+        assertEquals(2, list1.poll());
+        assertEquals(3, list1.poll());
+        assertEquals(4, list1.poll());
         
+        assertEquals(null, list1.poll());
+        assertEquals(0, list1.size());
     }
 
     @Test
     void append_to_itself(){
-        
+        assertThrows(IllegalArgumentException.class, () -> {
+            list1.append(list1);
+    });
     }
 }
