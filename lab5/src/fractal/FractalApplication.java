8d9a8c65af64098175d2450078a35334ed21796a
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-11-17 22:29:11 +0100

    Fin triangel

--- lab5/src/fractal/FractalApplication.java
+++ lab5/src/fractal/FractalApplication.java
@@ -6,7 +6,7 @@ import mountain.Mountain;
 public class FractalApplication {
 	public static void main(String[] args) {
 		Fractal[] fractals = new Fractal[2];
-		fractals[0] = new Mountain(300);
+		fractals[0] = new Mountain(null, null, null);
 		fractals[1] = new Koch(300);
 	    new FractalView(fractals, "Fraktaler", 600, 600);
 	}
--- lab5/src/mountain/Mountain.java
+++ lab5/src/mountain/Mountain.java
@@ -5,28 +5,42 @@ import fractal.TurtleGraphics;
 
 public class Mountain extends Fractal {
     private int length;
+    private Point a;
+    private Point b;
+    private Point c;
 
-    public Mountain(int length){
+    public Mountain(Point a, Point b, Point c){
         super();
-        this.length = length;
+        this.a = new Point(165, 396);
+        this.b = new Point(289, 157);
+        this.c = new Point(486, 433);
     }
 
     @Override
     public String getTitle() {
-        return "Bergfractal";
+        return "Bergfraktal";
     }
 
     @Override
     public void draw(TurtleGraphics turtle) {
-        // turtle.moveTo(turtle.getWidth() / 2.0 - length / 2.0,
-        // turtle.getHeight() / 2.0 - Math.sqrt(3.0) * length / 4.0); // Placera turtle.
-        Point a = new Point((int) (turtle.getWidth() / 2.0 - length / 2.0),
-        (int) (turtle.getHeight() / 2.0 - Math.sqrt(3.0) * length / 4.0));
+        // turtle.moveTo(a.getX(), a.getY());
+        // turtle.penDown();
+        // turtle.forwardTo(b.getX(), b.getY());
+        // turtle.forwardTo(c.getX(), c.getY());
+        // turtle.forwardTo(a.getX(), a.getY());
+        fractalLine(turtle, order, length, order);
+
         
     }
 
     private void fractalLine(TurtleGraphics turtle, int order, double length, double alpha){
         if(order == 0){
+            turtle.moveTo(a.getX(), a.getY());
+            turtle.penDown();
+            turtle.forwardTo(b.getX(), b.getY());
+            turtle.forwardTo(c.getX(), c.getY());
+            turtle.forwardTo(a.getX(), a.getY());
+        } else {
             
         }
     }
