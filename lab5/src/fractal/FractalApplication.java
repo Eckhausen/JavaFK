f6426fd713d76104aed83d1cf7468b2da0473b3f
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-11-18 12:51:06 +0100

    ADS

--- lab5/src/fractal/FractalApplication.java
+++ lab5/src/fractal/FractalApplication.java
@@ -2,11 +2,17 @@ package fractal;
 
 import koch.Koch;
 import mountain.Mountain;
+import mountain.Point;
 
 public class FractalApplication {
 	public static void main(String[] args) {
 		Fractal[] fractals = new Fractal[2];
-		fractals[0] = new Mountain(null, null, null);
+		
+		Point p1 = new Point(165, 396);
+		Point p2 = new Point(289, 157);
+		Point p3 = new Point(486, 433);
+
+		fractals[0] = new Mountain(300, p1, p2, p3);
 		fractals[1] = new Koch(300);
 	    new FractalView(fractals, "Fraktaler", 600, 600);
 	}
