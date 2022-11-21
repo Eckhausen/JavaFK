5406131a4b511522ae04a3c941040bc8d1fe864e
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-11-21 15:37:29 +0100

    D4 complete

--- lab5/src/mountain/Mountain.java
+++ lab5/src/mountain/Mountain.java
@@ -4,17 +4,17 @@ import fractal.Fractal;
 import fractal.TurtleGraphics;
 
 public class Mountain extends Fractal {
-    // private int length;
     private Point a;
     private Point b;
     private Point c;
+    private double dev; 
 
-    public Mountain(int length, Point p1, Point p2, Point p3){
+    public Mountain(int length, Point p1, Point p2, Point p3, double dev){
         super();
-        // this.length = length;
         this.a = p1;
         this.b = p2;
         this.c = p3;
+        this.dev = dev;
     }
 
     @Override
@@ -24,12 +24,12 @@ public class Mountain extends Fractal {
 
     @Override
     public void draw(TurtleGraphics turtle) {
-        fractalLine(turtle, order, a, b, c);
+        fractalLine(turtle, order, a, b, c, dev);
 
         
     }
 
-    private void fractalLine(TurtleGraphics turtle, int order, Point a, Point b, Point c){
+    private void fractalLine(TurtleGraphics turtle, int order, Point a, Point b, Point c, double dev){
         if(order == 0){
             turtle.moveTo(a.getX(), a.getY());
             turtle.forwardTo(b.getX(), b.getY());
@@ -37,15 +37,16 @@ public class Mountain extends Fractal {
             turtle.forwardTo(a.getX(), a.getY());
         } else {
 
-            Point AB = new Point(setNewX(a, b), setNewY(a, b));
-            Point BC = new Point(setNewX(b, c), setNewY(b, c));
-            Point AC = new Point(setNewX(a, c), setNewY(a, c));
+            Point AB = new Point(setNewX(a, b), setNewY(a, b, dev));
+            Point BC = new Point(setNewX(b, c), setNewY(b, c, dev));
+            Point AC = new Point(setNewX(a, c), setNewY(a, c, dev));
 
+            dev = dev/2;
             order--;
-            fractalLine(turtle, order, a, AB, AC); 
-            fractalLine(turtle, order, AB, b, BC);
-            fractalLine(turtle, order, AC, BC, c);
-            fractalLine(turtle, order, AB, BC, AC); //Mitten
+            fractalLine(turtle, order, a, AB, AC, dev); 
+            fractalLine(turtle, order, AB, b, BC, dev);
+            fractalLine(turtle, order, AC, BC, c, dev);
+            fractalLine(turtle, order, AB, BC, AC, dev); //Mitten
         }
 
     }
@@ -54,8 +55,9 @@ public class Mountain extends Fractal {
         return ((p2.getX() - p1.getX())/2) + p1.getX();
     }
 
-    private int setNewY(Point p1, Point p2) {
-        return ((p2.getY() - p1.getY())/2) + p1.getY();
+    private int setNewY(Point p1, Point p2, double dev) {
+       double randValue = RandomUtilities.randFunc(dev);
+        return (int) randValue + ((p2.getY() - p1.getY())/2) + p1.getY();
     }
 
     
