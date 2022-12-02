b0775cd7f9d1563beb6bd7972b17aaf476daa290
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-12-02 13:49:18 +0100

    First commit

--- lab6/src/bst/BinarySearchTree.java
+++ lab6/src/bst/BinarySearchTree.java
@@ -7,20 +7,24 @@ import java.util.Comparator;
 public class BinarySearchTree<E> {
   BinaryNode<E> root;  // Används också i BSTVisaulizer
   int size;            // Används också i BSTVisaulizer
-  private Comparator<E> ccomparator;
+  private Comparator<E> comparator;
     
 	/**
-	 * Constructs an empty binary search tree.
+	 * Constructs an empty binary search tree. 
 	 */
 	public BinarySearchTree() {
-		
+		comparator = (e1, e2) -> ((Comparable<E>) e1).compareTo(e2);
+		root = null;
+		size = 0;
 	}
 	
 	/**
 	 * Constructs an empty binary search tree, sorted according to the specified comparator.
 	 */
 	public BinarySearchTree(Comparator<E> comparator) {
-		
+		this.comparator = comparator;
+		root = null;
+		size = 0;
 	}
 
 	/**
@@ -37,7 +41,7 @@ public class BinarySearchTree<E> {
 	 * @return the height of the tree
 	 */
 	public int height() {
-		return 0;
+		return levels(root);
 	}
 	
 	/**
@@ -94,6 +98,14 @@ public class BinarySearchTree<E> {
 		private BinaryNode(E element) {
 			this.element = element;
 		}	
+	}
+
+	private int levels(BinaryNode<E> node){
+		if(node.left == null && node.right == null){
+			return 0;
+		} else {
+			return 1 + Math.max(levels(node.left), levels(node.right));		//1 + högsta subnod.
+		}
 	}
 	
 }
