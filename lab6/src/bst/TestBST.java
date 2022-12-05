a65db3a6c4029b7066de5c351a3d1b9e9a1f5420
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-12-05 14:31:21 +0100

    Second commit

---
+++ lab6/src/bst/TestBST.java
@@ -0,0 +1,40 @@
+package bst;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+// import bst.BinarySearchTree;
+
+public class TestBST {
+    
+    @BeforeEach
+    void setUp(){
+
+    }
+
+    @AfterEach
+    void tearDown(){
+
+    }
+
+    @Test
+    //Height
+    
+
+
+
+
+    @Test
+    //Size
+    @Test
+    //Clear
+    @Test
+    //Add
+    
+
+}
