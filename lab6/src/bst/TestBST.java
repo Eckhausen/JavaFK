77f4ce3ca18a4395417706b562adb3ca9e5085a2
Eknz350 <80269043+Eknz350@users.noreply.github.com>
2022-12-05 15:03:52 +0100

    Kl 15:00

--- lab6/src/bst/TestBST.java
+++ lab6/src/bst/TestBST.java
@@ -4,37 +4,49 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.Comparator;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-// import bst.BinarySearchTree;
+//  import bst.BinarySearchTree;
 
 public class TestBST {
-    
+    private BinarySearchTree<Integer> BstInt; 
+    private BinarySearchTree<String> BstString;
     @BeforeEach
     void setUp(){
-
+        //Sortera efter heltal
+        BstInt = new BinarySearchTree<>();
+        //Sortera alfabetiskt
+        BstString = new BinarySearchTree<>((e1, e2) -> Character.compare(e1.charAt(0), e2.charAt(0)));
     }
 
     @AfterEach
     void tearDown(){
+        BstInt = null;
+        BstString = null;
+    }
+
+    @Test
+    void testHeight(){
+        assertEquals(0, BstInt.height());
+        assertEquals(0, BstString.height());
+    }
+    
+    @Test
+    void testSize(){
 
     }
 
     @Test
-    //Height
-    
+    void testClear(){
 
-
-
+    }
 
     @Test
-    //Size
-    @Test
-    //Clear
-    @Test
-    //Add
-    
+    void testAdd(){
 
+    }
 }
